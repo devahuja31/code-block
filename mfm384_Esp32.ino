dc9c0a4b51bba190de406a727216f6846e5757f2
devahuja31 <62464853+devahuja31@users.noreply.github.com>
2020-03-23 14:16:25 +0530

    Update mfm384_Esp32.ino

--- mfm384_Esp32.ino
+++ mfm384_Esp32.ino
@@ -1,40 +1,8 @@
-/*
 
-  RS485_HalfDuplex.pde - example using ModbusMaster library to communicate
-  with EPSolar LS2024B controller using a half-duplex RS485 transceiver.
-
-  This example is tested against an EPSolar LS2024B solar charge controller.
-  See here for protocol specs:
-  http://www.solar-elektro.cz/data/dokumenty/1733_modbus_protocol.pdf
-
-  Library:: ModbusMaster
-  Author:: Marius Kintel <marius at kintel dot net>
-
-  Copyright:: 2009-2016 Doc Walker
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
-*/
 
 #include <ModbusMaster.h>
 //#include <SoftwareSerial.h>
 
-
-/*!
-  We're using a MAX485-compatible RS485 Transceiver.
-  Rx/Tx is hooked up to the hardware serial port at 'Serial'.
-  The Data Enable and Receiver Enable pins are hooked up as follows:
-*/
 #define MAX485_DE      2
 #define MAX485_RE_NEG  2
 
